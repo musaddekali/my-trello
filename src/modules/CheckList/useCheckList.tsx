50a7d8f6fc587f2ecb3e5ac27c99b9b94d0f678e
MusaddekAli <musaddekali.dev@gmail.com>
2023-04-09 11:54:22 +0600

    working on editor

--- src/modules/CheckList/useCheckList.tsx
+++ src/modules/CheckList/useCheckList.tsx
@@ -2,15 +2,28 @@ import { Form } from "antd";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-type Data = {
+type DataType = {
   id: string;
   text: string;
   isDone: boolean;
   isEdit: boolean;
 };
 
+// let x:DataType = {
+//     id: '343',
+//     text: 'dfdkfdlkf dl',
+//     isDone: false,
+//     isEdit: false,
+// }
+
+// let key: keyof DataType = 'text';
+
+// let value: string | boolean = 'mahid'
+
+// x[key] = value;
+
 const useCheckList = () => {
-  const [data, setData] = useState<Data[]>([]);
+  const [data, setData] = useState<DataType[]>([]);
   const [form] = Form.useForm();
 
   const addData = (values: { text: string }) => {
@@ -27,8 +40,30 @@ const useCheckList = () => {
     form.resetFields();
   };
 
-  function updateData(dataId: string, key: string, value: string | boolean) {
-    let updatedData = data.map((item) => {
+  function updateBooleanData(dataId: string, value: boolean) {
+    let updatedData: DataType[] = data.map((item) => {
+      if (item.id === dataId) {
+        item.isDone = value;
+      }
+      return item;
+    });
+    setData(updatedData);
+  }
+
+  function handleComplete(dataId: string, value: boolean): void {
+    updateBooleanData(dataId, value);
+  }
+
+  function handleEdit(dataId: string, value: boolean): void {
+    updateBooleanData(dataId, value);
+  }
+
+  function updateData(
+    dataId: string,
+    key: keyof DataType,
+    value: string | boolean
+  ) {
+    let updatedData: DataType[] = data.map((item) => {
       if (item.id === dataId) {
         item[key] = value;
       }
